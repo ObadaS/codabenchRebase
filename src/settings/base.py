d4aae40a8012fb20c58df9ed96676d954ae989d7
Obada Haddad <obada.haddad@lisn.fr>
2025-10-24 13:44:53 +0200

    circle-ci tests

--- src/settings/base.py
+++ src/settings/base.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = THIRD_PARTY_APPS + OUR_APPS
 
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
