8d73ee4624641229c06872158694ea7784832dde
Obada Haddad <obada.haddad@lisn.fr>
2025-11-14 16:59:09 +0100

    Removed useless line

--- compute_worker/compute_worker.py
+++ compute_worker/compute_worker.py
@@ -42,7 +42,6 @@ from logs_loguru import configure_logging
 # -----------------------------------------------
 # Initialize Docker or Podman depending on .env
 # -----------------------------------------------
-CONTAINER_ENGINE_EXECUTABLE="docker"
 if os.environ.get("CONTAINER_ENGINE_EXECUTABLE", "docker").lower() == "docker":
     client = docker.APIClient(base_url=os.environ.get("CONTAINER_SOCKET", "unix://var/run/docker.sock"), version="auto")
 elif os.environ.get("CONTAINER_ENGINE_EXECUTABLE").lower() == "podman":
